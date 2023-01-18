09742de44723e2b18ec633d777f39b9de3130d13
StepanPokhodzhai <stelkerstepan@gmail.com>
2023-01-18 15:27:31 +0200

    update parse
    added func parse

--- main.go
+++ main.go
@@ -41,18 +41,29 @@ func main() {
 
 	var digit, decimal, hundred int
 	fmt.Scan(&digit)
-	if digit >= 100 {
-		hundred = digit / 100
-		digit %= hundred * 100
-	}
-	if digit >= 10 {
-		decimal = digit / 10
-		digit %= decimal * 10
-	}
+	hundred, digit = parse(100, digit)
+	decimal, digit = parse(10, digit)
+	// if digit >= 100 {
+	// 	hundred = digit / 100
+	// 	digit %= hundred * 100
+	// }
+	// if digit >= 10 {
+	// 	decimal = digit / 10
+	// 	digit %= decimal * 10
+	// }
 
 	//hundredArr := [10]string{"", "сто ", "двісті ", "триста ", "чотириста ", "п'ятсот ", "шістсот ", "сімсот ", "вісімсот ", "дев'ятсот "}
 	//decimalAarr := [10]string{"", "десять ", "двадцять ", "тридцять ", "сорок ", "п'ятдесят ", "шістдесят ", "сімдесят ", "вісімдесят ", "дев'яносто "}
 	//digitArr := [10]string{"", "один", "два", "три", "чотири", "п'ять", "шість", "сім", "вісім", "дев'ять"}
 	//print(hundredArr[hundred], decimalAarr[decimal], digitArr[digit])
 	fmt.Print(numbers[hundred], numbers[decimal+10], numbers[digit+20])
+}
+
+func parse(check int, number int) (int, int) {
+	var res int
+	if number >= check {
+		res = number / check
+		number %= res * check
+	}
+	return res, number
 }
